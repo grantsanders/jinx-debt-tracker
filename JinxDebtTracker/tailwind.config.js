cb53de7a33396baf9c4955f1b7ab40290ed4d2cf
grantsanders <grantsanders89@gmail.com>
2024-12-25 15:42:09 -0500

    code cleanup

--- JinxDebtTracker/tailwind.config.js
+++ JinxDebtTracker/tailwind.config.js
@@ -3,8 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['**/*.{razor,html,cshtml,js}'],
-  plugins: [require('@tailwindcss/typography'), 
-    require('daisyui')],
+  plugins: [require('@tailwindcss/typography')],
   theme: {
     extend: {
       // Add custom animations here
