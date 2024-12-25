b71191d61b867271967a881f983e21b68a679c59
grantsanders <grantsanders89@gmail.com>
2024-12-25 15:03:00 -0500

    added loading wheel

--- JinxDebtTracker/tailwind.config.js
+++ JinxDebtTracker/tailwind.config.js
@@ -3,5 +3,23 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['**/*.{razor,html,cshtml,js}'],
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), 
+    require('daisyui')],
+  theme: {
+    extend: {
+      // Add custom animations here
+      colors: {
+        'dark-purple': '#6a0dad'  // Custom dark purple color
+      },
+      animation: {
+        'spin': 'spin 1s linear infinite',  // This creates the spin animation with infinite loop
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },  // Start of the animation
+          '100%': { transform: 'rotate(360deg)' }  // End of the animation (full rotation)
+        }
+      }
+    }
+  }
 }
